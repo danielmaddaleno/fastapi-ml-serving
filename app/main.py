09856a06cc833a65b044a987f29858365b90e13d
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2023-09-25 12:26:03 -0300

    feat: implement main

---
+++ app/main.py
@@ -0,0 +1,34 @@
+"""FastAPI ML serving application."""
+
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from app.models.registry import ModelRegistry
+from app.routes import health, predict
+from app.middleware.metrics import PrometheusMiddleware
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Load default model on startup, cleanup on shutdown."""
+    registry = ModelRegistry()
+    registry.load_default()
+    app.state.registry = registry
+    yield
+    registry.unload_all()
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="ML Serving API",
+        version="1.0.0",
+        lifespan=lifespan,
+    )
+    app.add_middleware(PrometheusMiddleware)
+    app.include_router(health.router, tags=["health"])
+    app.include_router(predict.router, tags=["inference"])
+    return app
+
+
+app = create_app()
