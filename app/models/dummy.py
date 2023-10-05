6ead53453a47132a73d95e17fd6ecd07a5fe1f95
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2023-10-05 14:06:05 -0300

    feat: implement dummy

---
+++ app/models/dummy.py
@@ -0,0 +1,10 @@
+"""Dummy model for testing – returns the mean of the feature vector."""
+
+import numpy as np
+
+
+class DummyModel:
+    """Sklearn-compatible predictor that returns np.mean(X, axis=1)."""
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        return np.mean(X, axis=1)
