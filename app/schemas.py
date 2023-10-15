b7a679c35291e3d02be0d30f8927d62b7a9e7bea
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2023-10-15 15:35:59 -0300

    feat: implement schemas

---
+++ app/schemas.py
@@ -0,0 +1,24 @@
+"""Pydantic request / response schemas."""
+
+from pydantic import BaseModel, Field
+
+
+class PredictionRequest(BaseModel):
+    features: list[float] = Field(
+        ..., min_length=1, description="Numeric feature vector"
+    )
+    model_version: str | None = Field(
+        None, description="Optional model version override"
+    )
+
+
+class PredictionResponse(BaseModel):
+    prediction: float
+    model_version: str
+    latency_ms: float
+
+
+class HealthResponse(BaseModel):
+    status: str
+    model_loaded: bool
+    version: str
