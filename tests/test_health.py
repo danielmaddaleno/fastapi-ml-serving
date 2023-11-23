309625c6ca89433bcf02dcc97c640a8e6d9bdf72
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2023-11-22 22:03:04 -0300

    test: add test health

---
+++ tests/test_health.py
@@ -0,0 +1,33 @@
+"""Tests for health & readiness endpoints."""
+
+import pytest
+from httpx import ASGITransport, AsyncClient
+
+from app.main import create_app
+
+
+@pytest.fixture
+def app():
+    return create_app()
+
+
+@pytest.mark.anyio
+async def test_health(app):
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        resp = await client.get("/health")
+    assert resp.status_code == 200
+    data = resp.json()
+    assert data["status"] == "ok"
+    assert data["model_loaded"] is True
+
+
+@pytest.mark.anyio
+async def test_ready(app):
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        resp = await client.get("/ready")
+    assert resp.status_code == 200
+    assert resp.json()["ready"] is True
